1a25fb2f54d07e759e7534b0aa44cc27e066dcdd
mahmood199 <gaudentas199@gmail.com>
2021-06-12 19:47:14 +0530

    Added some animations to fragments

--- app/src/main/java/com/example/resumie/PDF/PDFFragment.java
+++ app/src/main/java/com/example/resumie/PDF/PDFFragment.java
@@ -2,6 +2,7 @@ package com.example.resumie.PDF;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -11,6 +12,8 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.transition.TransitionInflater;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +27,8 @@ import com.example.resumie.R;
 
 public class PDFFragment extends Fragment {
 
+
+    public static final String TAG = "TAGPDFFragment";
     private EditText myEditText;
     Button button;
 
@@ -32,14 +37,23 @@ public class PDFFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+        Log.i(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
+
+
+        TransitionInflater inflater = TransitionInflater.from(requireContext());
+        setExitTransition(inflater.inflateTransition(R.transition.fade));
+
+        TransitionInflater inflater1 = TransitionInflater.from(requireContext());
+        setEnterTransition(inflater1.inflateTransition(R.transition.slide_right));
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_p_d_f, container, false);
         button=view.findViewById(R.id.pdfcreator);
-
+        Log.i(TAG, "onCreateView: ");
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -54,4 +68,73 @@ public class PDFFragment extends Fragment {
         });
         return view;
     }
+
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.i(TAG, "onDestroyView: ");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i(TAG, "onDetach: ");
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Log.i(TAG, "onViewStateRestored: ");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState: ");
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.i(TAG, "onAttach: ");
+    }
+
+
 }
--- app/src/main/java/com/example/resumie/portfolio/PortfolioDetailsFragment.java
+++ app/src/main/java/com/example/resumie/portfolio/PortfolioDetailsFragment.java
@@ -1,6 +1,9 @@
 package com.example.resumie.portfolio;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.transition.TransitionInflater;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +21,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 public class PortfolioDetailsFragment extends BottomSheetDialogFragment {
 
 
+    private static final String TAG = "TAGPortfolioDetails";
     private ImageView imgPortfolio;
     private TextView title,description;
 
@@ -31,13 +35,21 @@ public class PortfolioDetailsFragment extends BottomSheetDialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate: ");
 
+
+        TransitionInflater inflater = TransitionInflater.from(requireContext());
+        setExitTransition(inflater.inflateTransition(R.transition.fade));
+
+        TransitionInflater inflater1 = TransitionInflater.from(requireContext());
+        setEnterTransition(inflater1.inflateTransition(R.transition.slide_right));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Log.i(TAG, "onCreateView: ");
         return inflater.inflate(R.layout.fragment_portfolio_details, container, false);
     }
 
@@ -46,6 +58,7 @@ public class PortfolioDetailsFragment extends BottomSheetDialogFragment {
     public void onViewCreated(@NonNull View view,@Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Log.i(TAG, "onViewCreated: ");
         imgPortfolio = view.findViewById(R.id.portfolio_details_img);
         title = view.findViewById(R.id.portfolio_details_title);
         description = view.findViewById(R.id.portfolio_details_desc);
@@ -67,4 +80,64 @@ public class PortfolioDetailsFragment extends BottomSheetDialogFragment {
     }
 
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Log.i(TAG, "onAttach: ");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Log.i(TAG, "onViewStateRestored: ");
+    }
+
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState: ");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i(TAG, "onDetach: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.i(TAG, "onDestroyView: ");
+    }
 }
